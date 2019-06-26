516a6b2f8fac2737d38c980ddb684d73237c45fd
RylkovAlex <32511612+RylkovAlex@users.noreply.github.com>
2019-06-26 10:53:48 +0300

    упростил логику перемещения метки

--- js/map.js
+++ js/map.js
@@ -1,84 +1,76 @@
 'use strict';
 
 (function () {
-  // Карта и параметры меток
-  var TOP_BORDER = 130;
+  // Карта и параметры метки:
   var mapBlock = document.querySelector('.map');
   var pinMain = mapBlock.querySelector('.map__pin--main');
-  // TODO: понять как получать translate из CSS
-  var PIN_MAIN_MARKER_TRANSLATE_Y = -6; // transform у псевдоэлемента, для вычисления точных размеров метки
+  var PIN_MAIN_MARKER_TRANSLATE_Y = -6; // transform у псевдоэлемента, для вычисления точных размеров метки (TODO: понять как получать translate из CSS)
   var pinMainHeight = pinMain.offsetHeight + parseInt(getComputedStyle(pinMain, '::after').height, 10) + PIN_MAIN_MARKER_TRANSLATE_Y; // 81px
   var pinMainMoveContainer = document.querySelector('.map__pins');
 
-  pinMain.addEventListener('mousedown', function (mousedownEvt) {
-    moveElement(mousedownEvt, pinMain, pinMainMoveContainer);
-  });
+  // границы области, внутри которой планируется перемещение:
+  var TOP_BORDER = 130;
+  var LOWER_BORDER = 704 - pinMainHeight;
+  var RIGHT_BORDER = pinMainMoveContainer.offsetWidth - pinMain.offsetWidth;
 
-  // Вспомогательные функции:
+  // init startCoords
+  var startCoords = {
+    x: 0,
+    y: 0,
+  };
 
-  function moveElement(mousedownEvt, element, zone) {
-    // рассчёт координат контейнера, внутри которого планируется перемещение:
-    var moveZone = window.util.getCoords(zone);
-    // текущие координаты метки:
-    var elementCoords = window.util.getCoords(element);
-    // начальные координаты курсора
-    /* TODO: я не пойму, как их можно привязать к функции обработки перемещения, чтобы сделать её универсальной, здесь простая передача параметром не сработает, нужно как-то через замыкание делать;
-    пробовал использовать вместо этих координат .movementX и Y внутри обработчика mousemove, но тогда будут
-    проблемы на границах... в общем пока так оставил, буду думать*/
-    var cursorCoords = {
-      x: mousedownEvt.clientX,
-      y: mousedownEvt.clientY
-    };
-    // смещение курсора относительно левого-верхнего угла элемента
-    var shiftX = mousedownEvt.clientX - elementCoords.left;
-    var shiftY = mousedownEvt.clientY - elementCoords.top;
-    // рассчёт границ области перемещения:
-    moveZone.left = moveZone.left + shiftX;
-    moveZone.right = moveZone.right - pinMain.offsetWidth + shiftX;
-    moveZone.bottom = moveZone.bottom - pinMainHeight + shiftY;
-    moveZone.top = moveZone.top + shiftY + TOP_BORDER;
-    // подготовка к перемещению
-    element.style.position = 'absolute';
-    element.style.zIndex = 10;
+  pinMain.addEventListener('mousedown', pinMainMousedownHandler);
 
+  // -------------------
+
+  function pinMainMousedownHandler(mousedownEvt) {
+    // подготовка к перемещению:
+    pinMain.style.position = 'absolute';
+    pinMain.style.zIndex = 10;
+    // запомнили положение курсора
+    startCoords.x = mousedownEvt.clientX;
+    startCoords.y = mousedownEvt.clientY;
+    // добавили обработчики мыши
     document.addEventListener('mousemove', buttonMoveHandler);
-    document.addEventListener('mouseup', buttonMouseUpHandler);
+    document.addEventListener('mouseup', buttonMouseupHandler);
+  }
 
-    // функция для реализации перемещения:
-    function changeElementPosition(moveEvt, elem, left, right, bottom, top) {
-      moveEvt.preventDefault();
-      // расчёт смещения курсора
-      var shift = {
-        x: cursorCoords.x - moveEvt.clientX,
-        y: cursorCoords.y - moveEvt.clientY
-      };
-      // Если новое положение курсора попадает в границы области перемещения, то перемещаем метку:
-      if ((moveEvt.clientX > left) &&
-          (moveEvt.clientX < right) &&
-          (moveEvt.clientY < bottom) &&
-          (moveEvt.clientY > top)
-      ) {
-        // перемещение элемента на вычисленное смещение курсора
-        elem.style.top = (elem.offsetTop - shift.y) + 'px';
-        elem.style.left = (elem.offsetLeft - shift.x) + 'px';
-        // перезапись стартовых координат курсора на новые, текущие
-        cursorCoords = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
-        };
-      }
-    }
+  function buttonMouseupHandler() {
+    // убираем обработчики
+    document.removeEventListener('mousemove', buttonMoveHandler);
+    document.removeEventListener('mouseup', buttonMouseupHandler);
+  }
 
-    // обработчик mousemove:
-    function buttonMoveHandler(moveEvt) {
-      changeElementPosition(moveEvt, pinMain, moveZone.left, moveZone.right, moveZone.bottom, moveZone.top);
+  function buttonMoveHandler(moveEvt) {
+    moveEvt.preventDefault();
+    // расчёт смещения курсора относительно начального положения
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+    // меняем координаты курсора на новые (текущие)
+    startCoords.x = moveEvt.clientX;
+    startCoords.y = moveEvt.clientY;
+    // вычисляем новые координаты для пина
+    var offsetTop = pinMain.offsetTop - shift.y;
+    var offsetLeft = pinMain.offsetLeft - shift.x;
+    // проверка по оси Y
+    // если пин выше чем нам надо, то присваем координате Y верхнюю границу
+    if (offsetTop < TOP_BORDER) {
+      offsetTop = TOP_BORDER;
+      // если ниже, то нижнюю
+    } else if (offsetTop >= LOWER_BORDER) {
+      offsetTop = LOWER_BORDER;
     }
-    // обработчик mouseUp
-    function buttonMouseUpHandler() {
-      // удаление обработчиков
-      document.removeEventListener('mousemove', buttonMoveHandler);
-      document.removeEventListener('mouseup', buttonMouseUpHandler);
+    // аналогично для оси X
+    if (offsetLeft > RIGHT_BORDER) {
+      offsetLeft = RIGHT_BORDER;
+    } else if (offsetLeft < 0) {
+      offsetLeft = 0;
     }
+    // отрисовка смещенея пина через стили
+    pinMain.style.top = offsetTop + 'px';
+    pinMain.style.left = offsetLeft + 'px';
   }
 
 })();
